2ee3c8fb1a04b0c1f9da956ecd3a3301a2f715ca
Anna Anks Nowak <matisnape@wp.pl>
2018-01-30 22:37:34 +0100

    fixed config and working on updating failed status

--- test/test.js
+++ test/test.js
@@ -1,11 +1,21 @@
 describe('Netguru.co page', function() {
-    it('should have the right title', function () {
+
+    beforeEach(function() {
         browser.url('/');
+      });
+
+    it('should have the right title', function () {
         expect(browser.getTitle()).toBe('Building software for world changers | Netguru');
         browser.element("//div[@id='main-nav']//a[text()='About us']").click();
         browser.waitUntil(function () {
             return browser.getUrl().match('/about-us');
           }, 5000 );
         expect(browser.getTitle()).toBe('About us - Our approach');
+        console.log(test);
+    });
+
+    it('this test should fail', function () {
+        expect(browser.getTitle()).toBe('This is not the title');
+        console.log(test);
     });
 });
--- wdio.conf.js
+++ wdio.conf.js
@@ -3,25 +3,28 @@ exports.config = {
     user: process.env.BROWSERSTACK_USERNAME || BROWSERSTACK_USERNAME,
     key: process.env.BROWSERSTACK_ACCESS_KEY || BROWSERSTACK_ACCESS_KEY,
 
+    updateJob: false,
     specs: [
         './test/**/*.js'
     ],
     exclude: [],
     maxInstances: 1,
     capabilities: [
-        {
-            'os': 'Windows',
-            'os_version': '7',
-            'browser': 'IE',
-            'browser_version': '11.0',
-            'resolution': '1280x1024'
-        },
+        // {
+        //     'os': 'Windows',
+        //     'os_version': '7',
+        //     'browser': 'IE',
+        //     'browser_version': '11.0',
+        //     'resolution': '1280x1024',
+        //     build: 'webdriver-browserstack2'
+        // },
         {
             'os': 'OS X',
             'os_version': 'Sierra',
             'browser': 'Chrome',
             'browser_version': '56.0',
             'resolution': '1280x1024' ,
+            build: 'webdriver-browserstack2',
             'chromeOptions': {
                 'prefs': {
                     'credentials_enable_service': false,
@@ -45,13 +48,28 @@ exports.config = {
     waitforTimeout: 10000,
     connectionRetryTimeout: 90000,
     connectionRetryCount: 3,
-    services: ['browserstack'],
     framework: 'jasmine',
     reporters: ['dot'],
+    host: 'hub.browserstack.com',
     jasmineNodeOpts: {
         defaultTimeoutInterval: 10000*10*9000,
         expectationResultHandler: function(passed, assertion) {
             // do something
         }
+    },
+    afterTest: function (test) {
+        if (test.passed) {
+            return console.log(test);
+        } else if (test.failed) {
+            var request = require("request");
+            request({
+                uri: "https://" + BROWSERSTACK_USERNAME + ":" + BROWSERSTACK_ACCESS_KEY + "@api.browserstack.com/automate/sessions/<session-id>.json",
+                method:"PUT",
+                form:{
+                    "status":"error",
+                    "reason":"assertion failed"
+                }
+            });
+        }
     }
-}
+};
