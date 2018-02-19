506966605470b7d1da2f57d7dafbe25c00d52b93
Anna Anks Nowak <matisnape@wp.pl>
2018-02-19 13:15:51 +0100

    and I need to set viewport size

--- test/test.js
+++ test/test.js
@@ -5,17 +5,18 @@ describe('Netguru.co page', function() {
       });
 
     it('should have the right title', function () {
-        expect(browser.getTitle()).toBe('Building software for world changers | Netguru');
+        browser.setViewportSize({
+            width: 1200,
+            height: 800
+        });
         browser.element("//div[@id='main-nav']//a[text()='About us']").click();
         browser.waitUntil(function () {
             return browser.getUrl().match('/about-us');
           }, 5000 );
         expect(browser.getTitle()).toBe('About us - Our approach');
-        console.log(test);
     });
 
     it('this test should fail', function () {
         expect(browser.getTitle()).toBe('This is not the title');
-        console.log(test);
     });
 });
